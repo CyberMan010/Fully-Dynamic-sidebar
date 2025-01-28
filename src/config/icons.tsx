7ae7b17638e17fae32d8832ba65fd9cebdf67a22
CyberMan010 <mohamahasoun60@gmail.com>
2025-01-28 13:06:48 +0300

    feat: icons now adjusted in the icon config

--- src/config/icons.tsx
+++ src/config/icons.tsx
@@ -1,9 +1,25 @@
 export type IconName = 
-  | 'user'
-  | 'settings'
+  | 'add'
+  | 'arrange-square'
   | 'book'
-  | 'sidebar-left' 
-  | 'sidebar-right';
+  | 'briefcase'
+  | 'cmd-icon'
+  | 'cmd-icon2'
+  | 'hierarchy-square-3'
+  | 'home-2'
+  | 'key-square'
+  | 'Logo Icon'
+  | 'message-question'
+  | 'money-send'
+  | 'personalcard'
+  | 'profile-2user'
+  | 'receipt-search'
+  | 'setting-3'
+  | 'sidebar-left'
+  | 'sidebar-right'
+  | 'trade'
+  | 'user-octagon'
+ 
 
 
 interface IconProps {
