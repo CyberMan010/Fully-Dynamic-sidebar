ab8dd004dadc21c3115a2cb169157e56d6e332b6
CyberMan010 <mohamahasoun60@gmail.com>
2025-02-03 01:36:08 +0300

    fix: toolsbar adjusted, cards need to be with it

--- src/components/Cards-Frame/Cards/Card.tsx
+++ src/components/Cards-Frame/Cards/Card.tsx
@@ -1,3 +1,4 @@
+// KnowledgeCard.tsx
 import type React from "react"
 import "../../../styles/Cards.scss"
 import { Icon } from "../../../config/icons"
@@ -15,7 +16,7 @@ export const KnowledgeCard: React.FC<CardProps> = ({
   description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
   sections = 12,
   articles = 50,
-  isNew = true,
+  isNew = false,
 }) => {
   return (
     <div className="card">
@@ -24,25 +25,24 @@ export const KnowledgeCard: React.FC<CardProps> = ({
           <h4 className="card__subtitle">Knowledge Base</h4>
           {isNew && <span className="card__label">New Collection</span>}
         </div>
-        <Icon name="logoCard" className="card__logo" />
-      </div>
-      <h3 className="card__title">{title}</h3>
-      <p className="card__description">{description}</p>
-      <div className="card__meta">
-        <div className="card__meta-item">
-          <Icon name="folder-2" />
-          <span>
-            {sections} Section{sections !== 1 ? "s" : ""}
-          </span>
+        <div className="card__logo-wrapper">
+          <Icon name="logoCard" size={40} className="card__logo" />
         </div>
-        <div className="card__meta-item">
-          <Icon name="note-text" />
-          <span>
-            {articles} Article{articles !== 1 ? "s" : ""}
-          </span>
+      </div>
+      <div className="card__content">
+        <h3 className="card__title">{title}</h3>
+        <p className="card__description">{description}</p>
+        <div className="card__meta">
+          <div className="card__meta-item">
+            <Icon name="folder-2" size={20} />
+            <span>{sections} Section{sections !== 1 ? 's' : ''}</span>
+          </div>
+          <div className="card__meta-item">
+            <Icon name="note-text" size={20} />
+            <span>{articles} Article{articles !== 1 ? 's' : ''}</span>
+          </div>
         </div>
       </div>
     </div>
   )
 }
-
